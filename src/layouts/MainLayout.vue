2e9c0a76dc936bed974c48f63ce2f5635a6e109a
macbook-air <techcom.ec20@gmail.com>
2021-12-22 01:53:17 +0700

    update: code

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,30 +1,41 @@
 <template>
   <q-layout view="hHh lpR fff">
     <q-header reveal class="bg-light-blue-9 text-white">
-      <!-- <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>Title
-        </q-toolbar-title>
-      </q-toolbar>-->
-
-      <q-tabs no-caps inline-label align="center">
-        <q-route-tab icon="eva-home-outline" to="/" exact label="HOME" />
-        <q-route-tab icon="eva-file-text-outline" to="/Resume" label="RESUME" />
-        <q-route-tab icon="eva-email-outline" to="/Contact" label="CONTACT" />
+      <q-tabs no-caps inline-label align="right">
+        <q-route-tab
+          :class="routerName === '' && 'text-light-green-13'"
+          icon="eva-home-outline"
+          to="/"
+          :label="isMobile ? '' : 'Home'"
+        />
+        <q-route-tab
+          :class="routerName === 'Resume' && 'text-light-green-13'"
+          icon="eva-file-text-outline"
+          to="/Resume"
+          :label="isMobile ? '' : 'Resume'"
+        />
+        <q-route-tab
+          :class="routerName === 'Contact' && 'text-light-green-13'"
+          icon="eva-email-outline"
+          to="/Contact"
+          :label="isMobile ? '' : 'Contact'"
+        />
+        <q-separator dark vertical inset />
+        <q-btn
+          flat
+          round
+          dense
+          class="q-ma-sm"
+          :icon="!$q.dark.isActive ? 'eva-sun-outline' : 'eva-moon-outline'"
+          @click="onDarkMode"
+        />
       </q-tabs>
-      <!-- <div class="full-height" :class="$q.dark.isActive ? 'drawer_dark' : 'drawer_normal'"></div> -->
     </q-header>
 
     <q-page-container>
-      <!-- page container -->
-      <q-page class="row bg-grey-4">
-        <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" mode="out-in">
-          <router-view />
-        </transition>
+      <q-page class="row">
+        <router-view />
       </q-page>
-      <!-- page container end -->
     </q-page-container>
 
     <q-footer class="bg-grey-8 text-white" align="center">
@@ -32,10 +43,23 @@
       <q-icon name="far fa-heart" color="pink" class="q-mr-sm q-ml-sm"></q-icon>+
       <q-icon name="fab fa-vuejs" color="green" class="q-mr-sm"></q-icon>
       <!-- <span>&copy; {{ new Date().getFullYear() }}</span> -->
+      {{ isMobile }}
     </q-footer>
   </q-layout>
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted } from 'vue'
+import { Platform, LocalStorage, Dark } from 'quasar'
+import { useRouter } from 'vue-router';
+const router = useRouter();
 
+onMounted(() => Dark.set(LocalStorage.getItem('dark_mode') ?? false))
+const isMobile = computed<boolean>(() => (Boolean)(Platform.is.mobile))
+const onDarkMode = () => {
+  Dark.toggle()
+  LocalStorage.set('dark_mode', Dark.mode)
+}
+
+const routerName = computed(() => router.currentRoute.value.fullPath.replace('/', ''))
 </script>
