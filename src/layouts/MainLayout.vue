4d9f10dbe85ad47aa0fce0c794a5c2f002e45ed9
macbook-air <techcom.ec20@gmail.com>
2022-01-01 13:30:43 +0700

    change: url => resume online

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -46,7 +46,7 @@
       <router-view />
 
       <q-page-sticky position="bottom-right" :offset="[20, 20]">
-        <q-btn fab icon="las la-file-download" color="positive" href="https://www.cakeresume.com/pdf/s--0mh9GueAv2SyxyB0xPRvjQ--/BOE2b.pdf">
+        <q-btn fab icon="las la-file-download" color="positive" href="https://www.cakeresume.com/pdf/s--CWFP06rUR4ceuJ5j2nE37A--/GExZR.pdf">
           <q-tooltip>Download Resume</q-tooltip>
         </q-btn>
       </q-page-sticky>
