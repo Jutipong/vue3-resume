2b469fc31c884a1db64b823cc9379800639eed4a
macbook-air <techcom.ec20@gmail.com>
2022-01-01 16:12:21 +0700

    update folder img

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -8,25 +8,26 @@ import Title from '../components/Title.vue';
 import { ref } from 'vue';
 
 const photos = ref<Array<PhotoModel>>([
-    { Title: '.Net Core', Img: 'core.png' },
-    { Title: 'Entity', Img: 'entity.png' },
-    { Title: 'SQL Server', Img: 'sql-server.png' },
+    { Title: '.Net Core', Img: 'NetCore.png' },
+    { Title: 'C#', Img: 'Csharp.png' },
+    { Title: 'Entity', Img: 'Ef.png' },
+    { Title: 'SQL Server', Img: 'SqlServer.png' },
     { Title: 'Oracle', Img: 'Oracle.png' },
     { Title: 'SQLite', Img: 'SQLite.png' },
-    { Title: 'Go', Img: 'go.png' },
-    { Title: 'Go Fiber', Img: 'goFiber.png' },
-    { Title: 'Go Gin', Img: 'goGin.png' },
+    { Title: 'Go', Img: 'Go.png' },
+    { Title: 'Go Fiber', Img: 'GoFiber.png' },
+    { Title: 'Go Gin', Img: 'GoGin.png' },
     { Title: 'JavaScript', Img: 'JavaScript.png' },
     { Title: 'TypeScript', Img: 'TypeScript.png' },
     { Title: 'NodeJs', Img: 'NodeJs.png' },
-    { Title: 'NestJs', Img: 'nest.svg' },
-    { Title: 'Vue2 / Vue3', Img: 'vue.png' },
+    { Title: 'NestJs', Img: 'NestJs.svg' },
+    { Title: 'Vue2 / Vue3', Img: 'Vue.png' },
     { Title: 'Nuxt', Img: 'Nuxt.png' },
     { Title: 'Quasar', Img: 'Quasar.png' },
     { Title: 'Vuetify', Img: 'Vuetify.png' },
     { Title: 'Boostrap', Img: 'Bootstrap.png' },
-    { Title: 'HTML', Img: 'html.png' },
-    { Title: 'CSS', Img: 'css.png' },
+    { Title: 'HTML', Img: 'Html.png' },
+    { Title: 'CSS', Img: 'Css.png' },
     { Title: 'Git/Github', Img: 'Octocat.png' },
     { Title: 'Vim', Img: 'Vim.png' }]
 )
@@ -41,7 +42,7 @@ const photos = ref<Array<PhotoModel>>([
         </div>
         <div class="row items-center justify-center">
             <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1 col-xl-1 q-pa-sm" v-for="(item, index) in photos" :key="index">
-                <q-img :src="require(`assets/img/logo/${item.Img}`)">
+                <q-img :src="require(`assets/img/skills/${item.Img}`)">
                     <q-tooltip>{{ item.Title }}</q-tooltip>
                 </q-img>
             </div>
