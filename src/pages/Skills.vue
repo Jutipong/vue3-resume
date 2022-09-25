3e84aa563816d6f2256856dd362a60bf6a93e335
Jutipong.Dev <techcom.ec20@gmail.com>
2022-09-25 23:05:57 +0700

    update: skill

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -1,31 +1,22 @@
 <script lang="ts" setup>
+import Title from '@components/Title.vue';
+import { ref } from 'vue';
+
 interface PhotoModel {
     Title: string,
     Img: string
 }
-import Title from '@components/Title.vue';
-
-import { ref } from 'vue';
 
 const photos = ref<Array<PhotoModel>>([
     { Title: '.Net Core', Img: 'NetCore.png' },
     { Title: 'C#', Img: 'Csharp.png' },
     { Title: 'Entity', Img: 'Ef.png' },
     { Title: 'SQL Server', Img: 'SqlServer.png' },
-    { Title: 'Oracle', Img: 'Oracle.png' },
-    { Title: 'SQLite', Img: 'SQLite.png' },
     { Title: 'Go', Img: 'Go.png' },
-    { Title: 'Go Fiber', Img: 'GoFiber.png' },
-    { Title: 'Go Gin', Img: 'GoGin.png' },
     { Title: 'JavaScript', Img: 'JavaScript.png' },
     { Title: 'TypeScript', Img: 'TypeScript.png' },
     { Title: 'NodeJs', Img: 'NodeJs.png' },
-    { Title: 'NestJs', Img: 'NestJs.svg' },
     { Title: 'Vue2 / Vue3', Img: 'Vue.png' },
-    { Title: 'Nuxt', Img: 'Nuxt.png' },
-    { Title: 'Quasar', Img: 'Quasar.png' },
-    { Title: 'Vuetify', Img: 'Vuetify.png' },
-    { Title: 'Boostrap', Img: 'Bootstrap.png' },
     { Title: 'HTML', Img: 'Html.png' },
     { Title: 'CSS', Img: 'Css.png' },
     { Title: 'Git/Github', Img: 'Octocat.png' },
